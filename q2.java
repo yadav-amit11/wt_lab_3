a5abe1299d5e9c05bac35f94ce104bd91663f60c
yadav-amit11 <122402537+yadav-amit11@users.noreply.github.com>
2023-02-03 01:08:34 +0530

    uploaded files

---
+++ q2.java
@@ -0,0 +1,9 @@
+class entry
+{
+	public static void Main(String[] args) {
+		System.out.println("Amit Kr. Yadav");
+         System.out.println("2105690");
+          System.out.println("CSE-11");
+           System.out.println("B-Tech : CSE");
+	}
+}
